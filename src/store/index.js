e3f8e6942672c18979c4138630c846d8b2c48806
Joseph Capote <capotejoseph@gmail.com>
2023-12-17 12:56:16 +0800

    setup vuex and axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import actions from './actions'
+import state from './state'
+import getters from './getters'
+import mutations from './mutations'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: state,
+  actions: actions,
+  getters: getters,
+  mutations: mutations
 })
